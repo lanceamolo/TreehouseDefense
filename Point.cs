61813f922f8fd423a2c0822b33be2787a2194731
lanceamolo <lanceamolo808@yahoo.com>
2021-07-06 22:38:32 -0700

    Added Point class

---
+++ Point.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace TreehouseDefense
+{
+    class Point
+    {
+        public readonly int X;
+        public readonly int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
