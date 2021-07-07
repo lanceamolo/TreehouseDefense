10a4bf0bae65dca3f1dc9455a718cdadeaf9cedd
lanceamolo <lanceamolo808@yahoo.com>
2021-07-07 15:49:21 -0700

    Added 2 DistanceTo methods to Point class

--- Point.cs
+++ Point.cs
@@ -12,5 +12,15 @@ namespace TreehouseDefense
             X = x;
             Y = y;
         }
+
+        public int DistanceTo(int x, int y)
+        {
+            return (int)Math.Sqrt(Math.Pow(X-x, 2) + Math.Pow(Y-y, 2));
+        }
+        
+        public int DistanceTo(Point point)
+        {
+            return DistanceTo(point.X, point.Y);
+        }
     }
 }
